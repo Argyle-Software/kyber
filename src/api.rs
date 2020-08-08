4559242d1488fc8d7ecdf2fa0338982ba1c97345
MitchellBerry <17392435+MitchellBerry@users.noreply.github.com>
2020-08-08 21:39:52 +0800

    proper return values added to api functions/ cleaned up comments

--- src/api.rs
+++ src/api.rs
@@ -3,13 +3,9 @@ use crate::{
   rng::*,
   symmetric::*,
   verify::*,
-  indcpa::*
+  indcpa::*,
+  error::*
 };
-
-pub const CRYPTO_SECRETKEYBYTES: usize =  KYBER_SECRETKEYBYTES;
-pub const CRYPTO_PUBLICKEYBYTES: usize =  KYBER_PUBLICKEYBYTES;
-pub const CRYPTO_CIPHERTEXTBYTES: usize = KYBER_CIPHERTEXTBYTES;
-pub const CRYPTO_BYTES: usize =           KYBER_SSBYTES;
 
 /*************************************************
 * Name:        crypto_kem_keypair
@@ -22,15 +18,18 @@ pub const CRYPTO_BYTES: usize =           KYBER_SSBYTES;
 *
 * Returns 0 (success)
 **************************************************/
-
-// Todo: return result
-pub fn crypto_kem_keypair(pk: &mut[u8], sk: &mut[u8])
-{
-  indcpa_keypair(pk, sk);
+pub fn crypto_kem_keypair(pk: &mut[u8], sk: &mut[u8], seed: Option<([u8;32], [u8;32])>) { 
+  indcpa_keypair(pk, sk, seed);
   let end = KYBER_INDCPA_PUBLICKEYBYTES + KYBER_INDCPA_SECRETKEYBYTES;
-  sk[KYBER_INDCPA_SECRETKEYBYTES..end].clone_from_slice(&pk[..KYBER_INDCPA_PUBLICKEYBYTES]);
-  hash_h(&mut sk[KYBER_SECRETKEYBYTES-2*KYBER_SYMBYTES..], pk, KYBER_PUBLICKEYBYTES);
-  randombytes(&mut sk[KYBER_SECRETKEYBYTES-KYBER_SYMBYTES..]);        /* Value z for pseudo-random output on reject */
+  sk[KYBER_INDCPA_SECRETKEYBYTES..end].copy_from_slice(&pk[..KYBER_INDCPA_PUBLICKEYBYTES]);
+  
+  let pk_start = KYBER_SECRETKEYBYTES - (2 * KYBER_SYMBYTES);
+  hash_h(&mut sk[pk_start..], pk, KYBER_PUBLICKEYBYTES);
+
+  match seed {
+    None => randombytes(&mut sk[KYBER_SECRETKEYBYTES-KYBER_SYMBYTES..],KYBER_SYMBYTES),
+    Some(s) => sk[KYBER_SECRETKEYBYTES-KYBER_SYMBYTES..].copy_from_slice(&s.1)
+  }
 }
 
 /*************************************************
@@ -45,24 +44,34 @@ pub fn crypto_kem_keypair(pk: &mut[u8], sk: &mut[u8])
 *
 * Returns 0 (success)
 **************************************************/
-
-pub fn crypto_kem_enc(ct: &mut[u8], ss: &mut[u8], pk: &[u8])
-{
+pub fn crypto_kem_enc(ct: &mut[u8], ss: &mut[u8], pk: &[u8], seed: Option<&[u8]>) {
   let mut kr = [0u8; 2*KYBER_SYMBYTES];
   let mut buf = [0u8; 2*KYBER_SYMBYTES];
   let mut randbuf = [0u8; 2*KYBER_SYMBYTES];
-  randombytes(&mut randbuf);
-  hash_h(&mut buf, &randbuf, KYBER_SYMBYTES);                                        /* Don't release system RNG output */
 
-  hash_h(&mut buf[KYBER_SYMBYTES..], pk, KYBER_PUBLICKEYBYTES);                    /* Multitarget countermeasure for coins + contributory KEM */
+  match seed {
+    // Retreive OS randombytes
+    None => randombytes(&mut randbuf, KYBER_SYMBYTES),
+    // Deterministic randbuf for KAT's
+    Some(s) => randbuf[..KYBER_SYMBYTES].copy_from_slice(&s)
+  }
+
+  // Don't release system RNG output 
+  hash_h(&mut buf, &randbuf, KYBER_SYMBYTES);
+
+  // Multitarget countermeasure for coins + contributory KEM
+  hash_h(&mut buf[KYBER_SYMBYTES..], pk, KYBER_PUBLICKEYBYTES);
   hash_g(&mut kr, &buf, 2*KYBER_SYMBYTES);
 
-  indcpa_enc(ct, &buf, pk, &kr[KYBER_SYMBYTES..]);                              /* coins are in kr+KYBER_SYMBYTES */
+  // coins are in kr[KYBER_SYMBYTES..]
+  indcpa_enc(ct, &buf, pk, &kr[KYBER_SYMBYTES..]);
 
-  hash_h(&mut kr[KYBER_SYMBYTES..], ct, KYBER_CIPHERTEXTBYTES);                    /* overwrite coins in kr with H(c) */
-  kdf(ss, &kr, 2*KYBER_SYMBYTES as u64); 
+  // overwrite coins in kr with H(c) 
+  hash_h(&mut kr[KYBER_SYMBYTES..], ct, KYBER_CIPHERTEXTBYTES);
+
+  // hash concatenation of pre-k and H(c) to k
+  kdf(ss, &kr, 2*KYBER_SYMBYTES as u64);
 }
-
 
 /*************************************************
 * Name:        crypto_kem_dec
@@ -78,29 +87,33 @@ pub fn crypto_kem_enc(ct: &mut[u8], ss: &mut[u8], pk: &[u8])
 *
 * On failure, ss will contain a pseudo-random value.
 **************************************************/
-
-pub fn crypto_kem_dec(ss: &mut[u8], ct: &[u8], sk: &mut[u8])
-{
+pub fn crypto_kem_dec(ss: &mut[u8], ct: &[u8], sk: &[u8]) -> Result<(), KyberError> {
   let mut buf = [0u8; 2*KYBER_SYMBYTES];
   let mut kr = [0u8; 2*KYBER_SYMBYTES];
   let mut cmp = [0u8; KYBER_CIPHERTEXTBYTES];
-  let mut pk = [0u8; KYBER_PUBLICKEYBYTES];
+  let mut pk = [0u8; KYBER_INDCPA_PUBLICKEYBYTES + 2*KYBER_SYMBYTES];
+
   pk.copy_from_slice(&sk[KYBER_INDCPA_SECRETKEYBYTES..]);
 
   indcpa_dec(&mut buf, ct, sk);
   for i in 0..KYBER_SYMBYTES {
-    buf[KYBER_SYMBYTES+i] = sk[KYBER_SECRETKEYBYTES-2*KYBER_SYMBYTES+i];   /* Save hash by storing H(pk) in sk */
+    // Save hash by storing H(pk) in sk 
+    buf[KYBER_SYMBYTES+i] = sk[KYBER_SECRETKEYBYTES-2*KYBER_SYMBYTES+i];
   }
   hash_g(&mut kr, &buf, 2*KYBER_SYMBYTES);
-
-  indcpa_enc(&mut cmp, &buf, &pk, &kr[KYBER_SYMBYTES..]);         /* coins are in kr+KYBER_SYMBYTES */
+  // coins are in kr[KYBER_SYMBYTES..] 
+  indcpa_enc(&mut cmp, &buf, &pk, &kr[KYBER_SYMBYTES..]);
 
   let fail = verify(ct, &cmp, KYBER_CIPHERTEXTBYTES);
+  // overwrite coins in kr with H(c)
+  hash_h(&mut kr[KYBER_SYMBYTES..], ct, KYBER_CIPHERTEXTBYTES);
+  // Overwrite pre-k with z on re-encryption failure 
+  cmov(&mut kr, &sk[KYBER_SECRETKEYBYTES-KYBER_SYMBYTES..], KYBER_SYMBYTES, fail);
+  // hash concatenation of pre-k and H(c) to k 
+  kdf(ss, &kr, 2*KYBER_SYMBYTES as u64);
 
-  hash_h(&mut kr[KYBER_SYMBYTES..], ct, KYBER_CIPHERTEXTBYTES);                    /* overwrite coins in kr with H(c)  */
-
-  cmov(&mut kr, &mut sk[KYBER_SECRETKEYBYTES-KYBER_SYMBYTES..], KYBER_SYMBYTES, fail);  /* Overwrite pre-k with z on re-encryption failure */
-
-  kdf(ss, &kr, 2*KYBER_SYMBYTES as u64);                                           /* hash concatenation of pre-k and H(c) to k */
-
+  match fail {
+    0 => Ok(()),
+    _ => Err(KyberError::DecodeFail)
+  }
 }
