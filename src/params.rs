695246ba17d176a366712c958144ac84be7c6d49
MitchellBerry <17392435+MitchellBerry@users.noreply.github.com>
2020-08-11 06:19:59 +0800

    added 90s version param

--- src/params.rs
+++ src/params.rs
@@ -2,20 +2,27 @@
 #[cfg(feature = "512")]
 pub const KYBER_K: usize = 2;
 
-#[cfg(not(feature = "512"))]
-#[cfg(not(feature = "1024"))]
+#[cfg(not(any(feature = "512", feature = "1024")))]
 pub const KYBER_K: usize = 3; 
 
 #[cfg(feature = "1024")]
 pub const KYBER_K: usize = 4;
+
+#[cfg(not(feature = "90s"))]
+pub const KYBER_90S: bool = false;
+
+#[cfg(feature = "90s")]
+pub const KYBER_90S: bool = true;
 
 pub const KYBER_N: usize = 256;
 pub const KYBER_Q: usize = 3329;
 
 pub const KYBER_ETA: usize = 2;
 
-pub const KYBER_SYMBYTES: usize = 32;   /* size in bytes of hashes, and seeds */
-pub const KYBER_SSBYTES: usize =  32;   /* size in bytes of shared key */
+// size in bytes of hashes, and seeds
+pub const KYBER_SYMBYTES: usize = 32;
+// size in bytes of shared key 
+pub const KYBER_SSBYTES: usize =  32; 
 
 pub const KYBER_POLYBYTES: usize = 384;
 pub const KYBER_POLYVECBYTES: usize =  KYBER_K * KYBER_POLYBYTES;
@@ -25,11 +32,9 @@ pub const KYBER_POLYCOMPRESSEDBYTES: usize =     96;
 #[cfg(feature = "512")]
 pub const KYBER_POLYVECCOMPRESSEDBYTES: usize =  KYBER_K * 320;
 
-#[cfg(not(feature = "512"))]
-#[cfg(not(feature = "1024"))]
+#[cfg(not(any(feature = "512", feature = "1024")))]
 pub const KYBER_POLYCOMPRESSEDBYTES: usize =     128;
-#[cfg(not(feature = "512"))]
-#[cfg(not(feature = "1024"))]
+#[cfg(not(any(feature = "512", feature = "1024")))]
 pub const KYBER_POLYVECCOMPRESSEDBYTES: usize =  KYBER_K * 320;
 
 #[cfg(feature = "1024")]
@@ -37,11 +42,13 @@ pub const KYBER_POLYCOMPRESSEDBYTES: usize =     160;
 #[cfg(feature = "1024")]
 pub const KYBER_POLYVECCOMPRESSEDBYTES: usize = KYBER_K * 352;
 
-pub const KYBER_INDCPA_MSGBYTES: usize =       KYBER_SYMBYTES;
+// TODO: unused, remove maybe 90s version?
+// pub const KYBER_INDCPA_MSGBYTES: usize =       KYBER_SYMBYTES;
 pub const KYBER_INDCPA_PUBLICKEYBYTES: usize = KYBER_POLYVECBYTES + KYBER_SYMBYTES;
 pub const KYBER_INDCPA_SECRETKEYBYTES: usize = KYBER_POLYVECBYTES;
 pub const KYBER_INDCPA_BYTES: usize = KYBER_POLYVECCOMPRESSEDBYTES + KYBER_POLYCOMPRESSEDBYTES;
 
 pub const KYBER_PUBLICKEYBYTES: usize = KYBER_INDCPA_PUBLICKEYBYTES;
-pub const KYBER_SECRETKEYBYTES: usize =  KYBER_INDCPA_SECRETKEYBYTES +  KYBER_INDCPA_PUBLICKEYBYTES + 2*KYBER_SYMBYTES; /* 32 bytes of additional space to save H(pk) */
+// 32 bytes of additional space to save H(pk)
+pub const KYBER_SECRETKEYBYTES: usize =  KYBER_INDCPA_SECRETKEYBYTES +  KYBER_INDCPA_PUBLICKEYBYTES + 2*KYBER_SYMBYTES; 
 pub const KYBER_CIPHERTEXTBYTES: usize =  KYBER_INDCPA_BYTES;
