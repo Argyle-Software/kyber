0a02913efadcaa162eebd9af39a41d53dc13c6be
MitchellBerry <17392435+MitchellBerry@users.noreply.github.com>
2020-08-17 10:55:24 +0800

    module docs and examples

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,101 @@
 #![allow(clippy::many_single_char_names)]
 
+//! # Kyber
+//! 
+//! A pure rust implmentation of the Kyber algorithm with wasm compilation.
+//! 
+//! To select different security strengths from the default enable 
+//! in your `cargo.toml` the feature of `kyber512` or `kyber1024` 
+//! 
+//! ## Usage 
+//!
+//! ```
+//! use kyber::*;
+//! ```
+//! ##### KEM
+//! ```
+//! // Generate Keypair
+//! let keys = keypair();
+//! 
+//! // Encapsulate
+//! let (ct, ss1) = encapsulate(&keys.pubkey).unwrap();
+//! 
+//! // Decapsulate
+//! let ss2 = decapsulate(&ct, &keys.secret).unwrap();
+//! ```
+//! 
+//! ##### Key Exchange
+//! 
+//! ```
+//! // Initialise
+//!let mut eska = [0u8; KYBER_SECRETKEYBYTES];
+//!
+//!let mut uake_senda = [0u8; KEX_UAKE_SENDABYTES];
+//!let mut uake_sendb = [0u8; KEX_UAKE_SENDBBYTES];
+//!
+//!let mut tk = [0u8; KEX_SSBYTES];
+//!let mut ka = [0u8; KEX_SSBYTES];
+//!let mut kb = [0u8; KEX_SSBYTES];
+//!
+//!let alice_keys = keypair();
+//!let bob_keys = keypair();
+//! ```
+//! 
+//! ##### UAKE
+//! ```
+//!// Alice
+//!uake_init_a(
+//!  &mut uake_senda, 
+//!  &mut tk, 
+//!  &mut eska, 
+//!  &bob_keys.pubkey
+//!);
+//!// Bob
+//!uake_shared_b(
+//!  &mut uake_sendb, 
+//!  &mut kb, 
+//!  &uake_senda, 
+//!  &bob_keys.secret
+//!).unwrap();
+//!// Alice
+//!uake_shared_a(
+//!  &mut ka, 
+//!  &uake_sendb, 
+//!  &tk, 
+//!  &eska
+//!).unwrap();
+//!
+//!assert_eq!(ka, kb);
+//!```
+//! 
+//! ##### AKE
+//! ```
+//!  // Alice
+//!  ake_init_a(
+//!    &mut ake_senda, 
+//!    &mut tk, 
+//!    &mut eska, 
+//!    &bob_keys.pubkey
+//!  );
+//!  // Bob
+//!  ake_shared_b(
+//!    &mut ake_sendb, 
+//!    &mut kb, 
+//!    &ake_senda, 
+//!    &bob_keys.secret,
+//!    &alice_keys.pubkey
+//!  ).unwrap();
+//!  // Alice
+//!  ake_shared_a(
+//!    &mut ka, 
+//!    &ake_sendb, 
+//!    &tk, 
+//!    &eska,
+//!    &alice_keys.secret
+//!  ).unwrap();
+//!
+//!  assert_eq!(ka, kb);
+//! ```
 #[cfg(feature = "wasm")]
 mod wasm;
 // mod aes256;
@@ -26,14 +122,7 @@ pub use api::{
   crypto_kem_enc, 
   crypto_kem_dec
 };
-pub use kex::{
-  ake_init_a, 
-  ake_shared_a, 
-  ake_shared_b, 
-  uake_init_a, 
-  uake_shared_a, 
-  uake_shared_b
-};
+pub use kex::*;
 pub use error::KyberError;
 pub use params::{
   KYBER_PUBLICKEYBYTES, 
@@ -44,6 +133,20 @@ pub use params::{
   KYBER_90S
 };
 
+/// Generates a new public/private keypair
+/// 
+/// ### Example
+/// ```
+/// use kyber::*;
+/// 
+/// let keys = keypair();
+/// let publickey = keys.pubkey;
+/// let secretkey = keys.secret;
+/// dbg!(utils::encode_hex(&publickey));
+/// dbg!(utils::encode_hex(&secretkey));
+/// assert_eq!(publickey.len(), KYBER_PUBLICKEYBYTES);
+/// assert_eq!(secretkey.len(), KYBER_SECRETKEYBYTES);
+/// ```
 pub fn keypair() -> Keys {
   let mut pk = [0u8; KYBER_PUBLICKEYBYTES];
   let mut sk = [0u8; KYBER_SECRETKEYBYTES];
@@ -55,6 +158,16 @@ pub fn keypair() -> Keys {
   } 
 }
 
+/// Encapsulates a public key
+///
+///
+/// ### Example
+/// ```
+/// use kyber::*;
+/// 
+/// let keys = keypair();
+/// let (ct, ss) = encapsulate(&keys.pubkey).unwrap();
+/// ```
 pub fn encapsulate(pk: &[u8]) -> Result<([u8; KYBER_CIPHERTEXTBYTES], [u8; KYBER_SSBYTES]), KyberError> {
   if pk.len() != KYBER_PUBLICKEYBYTES {
     return Err(KyberError::EncodeFail)
@@ -65,6 +178,17 @@ pub fn encapsulate(pk: &[u8]) -> Result<([u8; KYBER_CIPHERTEXTBYTES], [u8; KYBER
   Ok((ct, ss))
 }
 
+/// Decapsulates ciphertext with a secret key
+///
+/// ### Example
+/// ```
+/// use kyber::*;
+/// 
+/// let keys = keypair();
+/// let (ct, ss1) = encapsulate(&keys.pubkey).unwrap();
+/// let ss2 = decapsulate(&ct, &keys.secret).unwrap();
+/// assert_eq!(ss1, ss2);
+/// ```
 pub fn decapsulate(ct: &[u8], sk: &[u8]) -> Result<[u8; KYBER_SSBYTES], KyberError> {
   let mut ss = [0u8; KYBER_SSBYTES];
   match api::crypto_kem_dec(&mut ss, ct, sk) {
@@ -73,21 +197,18 @@ pub fn decapsulate(ct: &[u8], sk: &[u8]) -> Result<[u8; KYBER_SSBYTES], KyberErr
   }
 }
 
+/// Contains the public/private keypair
 #[derive(Copy, Clone)]
 pub struct Keys{
     pub pubkey: [u8; KYBER_PUBLICKEYBYTES],
-    pub secret: [u8; KYBER_SECRETKEYBYTES],
-    pub ciphertext: [u8; KYBER_CIPHERTEXTBYTES],
-    pub shared_secret: [u8; KYBER_SSBYTES],
+    pub secret: [u8; KYBER_SECRETKEYBYTES]
 }
 
 impl Default for Keys {
   fn default() -> Self {
     Keys {
       pubkey: [0u8; KYBER_PUBLICKEYBYTES],
-      secret: [0u8; KYBER_SECRETKEYBYTES],
-      ciphertext: [0u8; KYBER_CIPHERTEXTBYTES],
-      shared_secret: [0u8; KYBER_SSBYTES]
+      secret: [0u8; KYBER_SECRETKEYBYTES]
     }
   }
 }
