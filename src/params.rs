0283db8e35ce905cc5a74b3279fe82881aa680ca
MitchellBerry <17392435+MitchellBerry@users.noreply.github.com>
2020-08-17 10:55:33 +0800

    module docs

--- src/params.rs
+++ src/params.rs
@@ -1,13 +1,17 @@
 // Security Strengths
-#[cfg(feature = "512")]
+#[cfg(feature = "kyber512")]
 pub const KYBER_K: usize = 2;
 
-#[cfg(not(any(feature = "512", feature = "1024")))]
+/// The algorithm security level
+/// 
+/// Defaults to 3, will change when kyber512 or kyber1024 is selected.
+#[cfg(not(any(feature = "kyber512", feature = "1024")))]
 pub const KYBER_K: usize = 3; 
 
 #[cfg(feature = "1024")]
 pub const KYBER_K: usize = 4;
 
+/// Whether 90s mode is being used
 #[cfg(not(feature = "90s"))]
 pub const KYBER_90S: bool = false;
 
@@ -21,7 +25,7 @@ pub const KYBER_ETA: usize = 2;
 
 // size in bytes of hashes, and seeds
 pub const KYBER_SYMBYTES: usize = 32;
-// size in bytes of shared key 
+/// Size of the shared key 
 pub const KYBER_SSBYTES: usize =  32; 
 
 pub const KYBER_POLYBYTES: usize = 384;
@@ -42,13 +46,14 @@ pub const KYBER_POLYCOMPRESSEDBYTES: usize =     160;
 #[cfg(feature = "1024")]
 pub const KYBER_POLYVECCOMPRESSEDBYTES: usize = KYBER_K * 352;
 
-// TODO: unused, remove maybe 90s version?
 // pub const KYBER_INDCPA_MSGBYTES: usize =       KYBER_SYMBYTES;
 pub const KYBER_INDCPA_PUBLICKEYBYTES: usize = KYBER_POLYVECBYTES + KYBER_SYMBYTES;
 pub const KYBER_INDCPA_SECRETKEYBYTES: usize = KYBER_POLYVECBYTES;
 pub const KYBER_INDCPA_BYTES: usize = KYBER_POLYVECCOMPRESSEDBYTES + KYBER_POLYCOMPRESSEDBYTES;
 
+/// Size of the public key
 pub const KYBER_PUBLICKEYBYTES: usize = KYBER_INDCPA_PUBLICKEYBYTES;
-// 32 bytes of additional space to save H(pk)
+/// Size of the secret key 
 pub const KYBER_SECRETKEYBYTES: usize =  KYBER_INDCPA_SECRETKEYBYTES +  KYBER_INDCPA_PUBLICKEYBYTES + 2*KYBER_SYMBYTES; 
+/// Size of the ciphertext
 pub const KYBER_CIPHERTEXTBYTES: usize =  KYBER_INDCPA_BYTES;
