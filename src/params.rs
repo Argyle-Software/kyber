8521651e05d3353e6c8fe880fcd6234c81b5ba80
Mitchell Berry <foss@mitchellberry.com>
2020-12-21 15:05:03 +0800

    feature settings modified

--- src/params.rs
+++ src/params.rs
@@ -2,59 +2,65 @@
 #[cfg(feature = "kyber512")]
 pub const KYBER_K: usize = 2;
 
-/// The algorithm security level
+/// The security level of Kyber
 /// 
-/// Defaults to 3, will change when kyber512 or kyber1024 is selected.
-#[cfg(not(any(feature = "kyber512", feature = "1024")))]
+/// Defaults to 3 (kyber768), will be 2 or 4 repsectively when 
+/// kyber512 or kyber1024 are selected.
+#[cfg(not(any(feature = "kyber512", feature = "kyber1024")))]
 pub const KYBER_K: usize = 3; 
 
-#[cfg(feature = "1024")]
+#[cfg(feature = "kyber1024")]
 pub const KYBER_K: usize = 4;
 
-/// Whether 90s mode is being used
+/// A boolean flag for whether 90s mode is activated
+/// 
+/// In 90s mode AES-CTR and SHA256 primitives are used instead
+/// which in some instances can be useful for hardware supported
+/// improvements.
+/// 
+/// Defaults to false, set`features = ["90s"]` to use. 
 #[cfg(not(feature = "90s"))]
 pub const KYBER_90S: bool = false;
-
 #[cfg(feature = "90s")]
 pub const KYBER_90S: bool = true;
 
-pub const KYBER_N: usize = 256;
-pub const KYBER_Q: usize = 3329;
+
+pub(crate) const KYBER_N: usize = 256;
+pub(crate) const KYBER_Q: usize = 3329;
 
 #[cfg(feature = "kyber512")]
-pub const KYBER_ETA1: usize = 3;
+pub(crate) const KYBER_ETA1: usize = 3;
 #[cfg(not(feature = "kyber512"))]
-pub const KYBER_ETA1: usize = 2;
-
-pub const KYBER_ETA2: usize = 2;
+pub(crate) const KYBER_ETA1: usize = 2;
+pub(crate) const KYBER_ETA2: usize = 2;
 
 // size in bytes of hashes, and seeds
-pub const KYBER_SYMBYTES: usize = 32;
+pub(crate) const KYBER_SYMBYTES: usize = 32;
 /// Size of the shared key 
 pub const KYBER_SSBYTES: usize =  32; 
 
-pub const KYBER_POLYBYTES: usize = 384;
-pub const KYBER_POLYVECBYTES: usize =  KYBER_K * KYBER_POLYBYTES;
+pub(crate) const KYBER_POLYBYTES: usize = 384;
+pub(crate) const KYBER_POLYVECBYTES: usize =  KYBER_K * KYBER_POLYBYTES;
 
-#[cfg(feature = "512")]
-pub const KYBER_POLYCOMPRESSEDBYTES: usize =     96;
-#[cfg(feature = "512")]
-pub const KYBER_POLYVECCOMPRESSEDBYTES: usize =  KYBER_K * 320;
+#[cfg(feature = "kyber512")]
+pub(crate) const KYBER_POLYCOMPRESSEDBYTES: usize =     128;
 
-#[cfg(not(any(feature = "512", feature = "1024")))]
-pub const KYBER_POLYCOMPRESSEDBYTES: usize =     128;
-#[cfg(not(any(feature = "512", feature = "1024")))]
-pub const KYBER_POLYVECCOMPRESSEDBYTES: usize =  KYBER_K * 320;
+#[cfg(feature = "kyber512")]
+pub(crate) const KYBER_POLYVECCOMPRESSEDBYTES: usize =  KYBER_K * 320;
 
-#[cfg(feature = "1024")]
-pub const KYBER_POLYCOMPRESSEDBYTES: usize =     160;
-#[cfg(feature = "1024")]
-pub const KYBER_POLYVECCOMPRESSEDBYTES: usize = KYBER_K * 352;
+#[cfg(not(any(feature = "kyber512", feature = "kyber1024")))]
+pub(crate) const KYBER_POLYCOMPRESSEDBYTES: usize =     128;
+#[cfg(not(any(feature = "kyber512", feature = "kyber1024")))]
+pub(crate) const KYBER_POLYVECCOMPRESSEDBYTES: usize =  KYBER_K * 320;
 
-// pub const KYBER_INDCPA_MSGBYTES: usize =       KYBER_SYMBYTES;
-pub const KYBER_INDCPA_PUBLICKEYBYTES: usize = KYBER_POLYVECBYTES + KYBER_SYMBYTES;
-pub const KYBER_INDCPA_SECRETKEYBYTES: usize = KYBER_POLYVECBYTES;
-pub const KYBER_INDCPA_BYTES: usize = KYBER_POLYVECCOMPRESSEDBYTES + KYBER_POLYCOMPRESSEDBYTES;
+#[cfg(feature = "kyber1024")]
+pub(crate) const KYBER_POLYCOMPRESSEDBYTES: usize =     160;
+#[cfg(feature = "kyber1024")]
+pub(crate) const KYBER_POLYVECCOMPRESSEDBYTES: usize = KYBER_K * 352;
+
+pub(crate) const KYBER_INDCPA_PUBLICKEYBYTES: usize = KYBER_POLYVECBYTES + KYBER_SYMBYTES;
+pub(crate) const KYBER_INDCPA_SECRETKEYBYTES: usize = KYBER_POLYVECBYTES;
+pub(crate) const KYBER_INDCPA_BYTES: usize = KYBER_POLYVECCOMPRESSEDBYTES + KYBER_POLYCOMPRESSEDBYTES;
 
 /// Size of the public key
 pub const KYBER_PUBLICKEYBYTES: usize = KYBER_INDCPA_PUBLICKEYBYTES;
