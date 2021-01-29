3864e71a06919b0c7298f3b16d6720cd8ae47314
Mitchell Berry <foss@mitchellberry.com>
2021-01-29 16:09:25 +0800

    add avx2 tests

--- tests/run_all_tests.sh
+++ tests/run_all_tests.sh
@@ -1,13 +1,21 @@
 #!/bin/env bash
 set -e
 
-# kyber764
-cargo test --features "KATs"
-cargo test --features "KATs 90s"
+cargo test --features "KATs" # kyber764
+cargo test --features "KATs 90s" # kyber764
 
 cargo test --features "KATs kyber512"
 cargo test --features "KATs kyber512 90s"
 
 cargo test --features "KATs kyber1024"
 cargo test --features "KATs kyber1024 90s"
+
+cargo test --features "avx2 KATs"
+# cargo test --features "avx2 KATs 90s"
+
+cargo test --features "avx2 KATs kyber512"
+# cargo test --features "avx2 KATs kyber512 90s"
+
+cargo test --features "avx2 KATs kyber1024"
+# cargo test --features "avx2 KATs kyber1024 90s"
 
