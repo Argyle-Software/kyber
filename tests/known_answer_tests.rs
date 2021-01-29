b5a57b4e6810bd16f6ab48ebdf77f26874ef2052
Mitchell Berry <foss@mitchellberry.com>
2021-01-29 16:07:52 +0800

    rename tests

--- tests/known_answer_tests.rs
+++ tests/known_answer_tests.rs
@@ -1,14 +1,14 @@
 mod load;
 
-use load::*;
 use pqc_kyber::*;
+use load::*;
 
 // Generate KAT keypairs from seeds.
 #[test]
 #[cfg(feature="KATs")]
 fn keypairs() {
   let kats = build_kats();
-  let mut _rng = rand::thread_rng(); // no-op for KATs
+  let mut _rng = rand::thread_rng(); // placeholder
   for kat in kats {
     let known_pk = decode_hex(&kat.pk);
     let known_sk = decode_hex(&kat.sk);
@@ -18,18 +18,19 @@ fn keypairs() {
     let mut pk = [0u8; KYBER_PUBLICKEYBYTES];
     let mut sk = [0u8; KYBER_SECRETKEYBYTES];
     let res = crypto_kem_keypair(&mut pk, &mut sk, &mut _rng, bufs);
-    assert!(res.is_ok());
-    assert_eq!(&pk[..], &known_pk[..], "Public key generation failure");
-    assert_eq!(&sk[..], &known_sk[..], "Secret key generation failure");
+    assert!(res.is_ok(), "Keypair generation failure");
+    assert_eq!(&pk[..], &known_pk[..], "Public key KAT mismatch");
+    assert_eq!(&sk[..], &known_sk[..], "Secret key KAT mismatch");
   }
 }
 
+
 // Encapsulating KAT's using deterministic rand buffers
 #[test]
-#[cfg(all(feature="KATs"))]
+#[cfg(feature="KATs")]
 fn encaps() {
   let kats = build_kats();
-  let mut _rng = rand::thread_rng(); // no-op for KATs
+  let mut _rng = rand::thread_rng(); // placeholder
   for kat in kats {
     let known_ct = decode_hex(&kat.ct);
     let known_ss = decode_hex(&kat.ss);
@@ -39,22 +40,24 @@ fn encaps() {
     let mut ct = [0u8; KYBER_CIPHERTEXTBYTES];
     let mut ss = [0u8; KYBER_SSBYTES];
     let res = crypto_kem_enc(&mut ct, &mut ss, &pk, &mut _rng, encap_buf);
-    assert!(res.is_ok());
-    assert_eq!(&ct[..], &known_ct[..], "Ciphertext creation failure");
-    assert_eq!(&ss[..], &known_ss[..], "Shared secret creation failure");
+    assert!(res.is_ok(), "KEM encapsulation failure");
+    assert_eq!(&ct[..], &known_ct[..], "Ciphertext KAT mismatch");
+    assert_eq!(&ss[..], &known_ss[..], "Shared secret KAT mismatch");
   }
 }
 
 // Decapsulating KAT's
 #[test]
-#[cfg(all(feature="KATs"))]
+#[cfg(feature="KATs")]
 fn decaps() {
   let kats = build_kats();
   for kat in kats {
     let sk = decode_hex(&kat.sk);
     let ct = decode_hex(&kat.ct);
+    let known_ss = decode_hex(&kat.ss);
     let decap_result = decapsulate(&ct, &sk);
-    assert!(decap_result.is_ok(), "KEM decasulation failure");
+    assert!(decap_result.is_ok(), "KEM decapsulation failure");
+    assert_eq!(&decap_result.unwrap()[..], &known_ss[..], "Shared secret KAT doesn't match")
   }
 }
 
@@ -67,7 +70,7 @@ pub fn encode_hex(bytes: &[u8]) -> String {
   output
 }
 
-// Decodes a hex string into a vector of bytes
+// Decodes hex string into a vector of bytes
 pub fn decode_hex(s: &str) -> Vec<u8> {
   (0..s.len())
       .step_by(2)
