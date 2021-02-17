abeb0d3c45dfd23731de7eb2c57578f174a2413a
Mitch Berry <foss@mitchellberry.com>
2021-02-17 12:23:12 +0800

    Made avx2 feature default on x86 platforms Added reference feature flag to force ref code usage

--- tests/run_all_tests.sh
+++ tests/run_all_tests.sh
@@ -1,21 +1,25 @@
 #!/bin/env bash
 set -e
 
+# Tests intended to be run on x86_64
+# avx2 90s mode not yet implemented
+
 cargo test --features "KATs" # kyber764
-cargo test --features "KATs 90s" # kyber764
+# cargo test --features "KATs 90s" # kyber764
 
 cargo test --features "KATs kyber512"
-cargo test --features "KATs kyber512 90s"
+# cargo test --features "KATs kyber512 90s"
 
 cargo test --features "KATs kyber1024"
-cargo test --features "KATs kyber1024 90s"
+# cargo test --features "KATs kyber1024 90s"
 
-cargo test --features "avx2 KATs"
-# cargo test --features "avx2 KATs 90s"
+cargo test --features "reference KATs"
+cargo test --features "reference KATs 90s"
 
-cargo test --features "avx2 KATs kyber512"
-# cargo test --features "avx2 KATs kyber512 90s"
+cargo test --features "reference KATs kyber512"
+cargo test --features "reference KATs kyber512 90s"
 
-cargo test --features "avx2 KATs kyber1024"
-# cargo test --features "avx2 KATs kyber1024 90s"
+cargo test --features "reference KATs kyber1024"
+cargo test --features "reference KATs kyber1024 90s"
+
 
