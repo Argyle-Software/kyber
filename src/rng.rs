4fdd97902b83eac55585055103c802e0314d48c5
Mitch Berry <foss@mitchellberry.com>
2021-05-20 12:48:08 +0800

    Add feature gate

--- src/rng.rs
+++ src/rng.rs
@@ -1,6 +1,7 @@
+#![cfg(not(feature="KATs"))]
 use rand_core::*;
 
-// Fills buffer x with len bytes, must satisfy the 
+// Fills buffer x with len bytes, RNG must satisfy the 
 // RngCore trait and CryptoRng marker trait requirements
 pub fn randombytes<R>(x: &mut [u8], len: usize, rng: &mut R)
   where R: RngCore + CryptoRng,
