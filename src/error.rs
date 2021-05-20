82ab19d222a9101faa4f2aae68881e2b42ccc634
Mitch Berry <foss@mitchellberry.com>
2021-05-20 03:48:16 +0000

    added invalid input error

--- src/error.rs
+++ src/error.rs
@@ -2,6 +2,7 @@
 #[derive(Debug, PartialEq)]
 /// Error type for the various failure modes
 pub enum KyberError {
+  InvalidInput,
   Encapsulation,
   Decapsulation,
   Rng
@@ -10,8 +11,9 @@ pub enum KyberError {
 impl core::fmt::Display for KyberError {
   fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
     match *self {
+      KyberError::InvalidInput => write!(f, "Function input is of incorrect length"),
       KyberError::Encapsulation => write!(f, "Encapsulation Failure"),
-      KyberError::Decapsulation => write!(f, "Decapsulation Failure"),
+      KyberError::Decapsulation => write!(f, "Decapsulation Failure, unable to obtain shared secret from ciphertext"),
       KyberError::Rng => write!(f, "RNG Failure"),
     }
   }
