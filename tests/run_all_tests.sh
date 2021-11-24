ba6d1be9e2b9000f387d3ba39f99083504d8935b
Mitch Berry <foss@mitchellberry.com>
2021-11-24 13:45:22 +0200

    Portable test matrix

--- tests/run_all_tests.sh
+++ tests/run_all_tests.sh
@@ -8,7 +8,9 @@ set -e
 # Enable LLVM address sanitser checks
 export RUSTFLAGS="-Z sanitizer=address -C target-cpu=native -C target-feature=+aes,+avx2,+sse2,+sse4.1,+bmi2,+popcnt"
 export RUSTDOCFLAGS="-Z sanitizer=address"
+TARGET=$(rustc -vV | sed -n 's|host: ||p')
 
+# Required for address sanitiser checks
 rustup default nightly
 
 # Print Headers
@@ -21,43 +23,43 @@ announce(){
 ##############################################################
 
 # Initial compile
-cargo build --tests --features "kyber512" 
+cargo build --tests --features "kyber512" --target $TARGET
 
 announce "Kyber512"
-cargo test --features "kyber512" 
+cargo test --features "kyber512" --target $TARGET
 
 announce "Kyber764"
-cargo test 
+cargo test --target $TARGET
 
 announce "Kyber1024"
-cargo test --features "kyber1024" 
+cargo test --features "kyber1024" --target $TARGET
 
 announce "Kyber512-90s"
-cargo test --features "kyber512 90s" 
+cargo test --features "kyber512 90s" --target $TARGET
 
 announce "Kyber764-90s"
-cargo test --features "90s" 
+cargo test --features "90s" --target $TARGET
 
 announce "Kyber1024-90s"
-cargo test --features "kyber1024 90s" 
+cargo test --features "kyber1024 90s" --target $TARGET
 
 announce "Reference Kyber512"
-cargo test --features "reference kyber512" 
+cargo test --features "reference kyber512" --target $TARGET
 
 announce "Reference Kyber764"
-cargo test --features "reference" 
+cargo test --features "reference" --target $TARGET
 
 announce "Reference Kyber1024"
-cargo test --features "reference kyber1024" 
+cargo test --features "reference kyber1024" --target $TARGET
 
 announce "Reference Kyber764-90s"
-cargo test --features "reference 90s" 
+cargo test --features "reference 90s" --target $TARGET
 
 announce "Reference Kyber512-90s"
-cargo test --features "reference kyber512 90s" 
+cargo test --features "reference kyber512 90s" --target $TARGET
 
 announce "Reference Kyber1024-90s"
-cargo test --features "reference kyber1024 90s" 
+cargo test --features "reference kyber1024 90s" --target $TARGET
 
 
 ##############################################################
@@ -65,7 +67,7 @@ cargo test --features "reference kyber1024 90s"
 # Omit santiser for faster test vectors
 export RUSTFLAGS="-C target-cpu=native -C target-feature=+aes,+avx2,+sse2,+sse4.1,+bmi2,+popcnt"
 
-cargo build --test kat --features "KATs kyber512" 
+cargo build --test kat --features "KATs kyber512" --target 
 
 announce "Kyber512 KATs"
 cargo test --test kat --features "KATs kyber512" 
